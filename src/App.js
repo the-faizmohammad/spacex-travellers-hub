70840a3ee0e88ec432c4172dac2a6cc4daeca3b4
user-faiz <email2faizm@gmail.com>
2023-11-07 15:13:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Line from './components/Line';
-import Rockets from './components/Rockets';
-import MyProfile from './components/MyProfile';
-import Missions from './components/Missions/Missions';
-import Dragons from './components/Dragons';
+import Rockets from './components/rockets';
+import MyProfile from './components/my-profile';
+import Missions from './components/missions';
+import Dragons from './components/dragons';
 
 function App() {
   return (
