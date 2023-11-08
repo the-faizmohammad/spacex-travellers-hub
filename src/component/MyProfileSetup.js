6a256f6af191ea909d0baed3d51b609da11fe125
Badru Ndawula <badrundawula24@gmail.com>
2023-11-08 11:16:36 +0300

    fix linters

--- src/component/MyProfileSetup.js
+++ src/component/MyProfileSetup.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const MyProfileSetup = () => {
-  return (
-    <div>
-      {/* Your content for the My Profile - Setup page will go here */}
-    </div>
-  );
-};
+const MyProfileSetup = () => (
+  <div>
+    {/* Your content for the My Profile - Setup page will go here */}
+  </div>
+);
 
 export default MyProfileSetup;
