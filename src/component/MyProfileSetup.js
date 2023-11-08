4bc8ce0addc97f160d55710f0d881a59449febf3
Badru Ndawula <badrundawula24@gmail.com>
2023-11-08 11:03:24 +0300

    empty profile setup

---
+++ src/component/MyProfileSetup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProfileSetup = () => {
+  return (
+    <div>
+      {/* Your content for the My Profile - Setup page will go here */}
+    </div>
+  );
+};
+
+export default MyProfileSetup;
