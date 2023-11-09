bdcc6ee47736eaba65daac25cacb34c144416d46
Obimbo07 <obimboausts@gmail.com>
2023-11-09 18:04:38 +0300

    Join-missionActions

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import '../styles/Mission.css';
@@ -6,18 +5,21 @@ import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
-import { fetchMissions } from '../redux/missionSlice';
+import { fetchMissions, reserveMission } from '../redux/missionSlice';
 
 const Mission = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.mission.data);
   const isLoading = useSelector((state) => state.mission.isloading);
-  const [checked, setChecked] = useState(false);
   const [statusText] = useState('Not a member');
 
   useEffect(() => {
     dispatch(fetchMissions());
   }, [dispatch]);
+
+  const handleJoinMission = (id) => {
+    dispatch(reserveMission({ id }));
+  };
 
   return (
     <div>
@@ -30,44 +32,46 @@ const Mission = () => {
             <thead>
               <tr>
                 <th>Mission</th>
-                <th>description</th>
+                <th>Description</th>
                 <th>Status</th>
-                <th>button</th>
+                <th>Button</th>
               </tr>
             </thead>
             <tbody>
-              {missions.map((mission) => (
-                <tr key={mission.mission_id}>
-                  <td>{mission.mission_name}</td>
-                  <td>{mission.description}</td>
-                  <td>
-                    <ToggleButton
-                      className="mb-2"
-                      id="toggle-check"
-                      type="checkbox"
-                      variant="dark"
-                      checked={checked}
-                      value="1"
-                      onChange={(e) => setChecked(e.currentTarget.checked)}
-                    >
-                      {statusText}
-                    </ToggleButton>
-                  </td>
-                  <td>
-                    <ButtonGroup className="mb-2">
+              {missions && missions.length > 0 ? (
+                missions.map((mission) => (
+                  <tr key={mission.mission_id}>
+                    <td>{mission.mission_name}</td>
+                    <td>{mission.description}</td>
+                    <td>
                       <ToggleButton
+                        className="mb-2"
                         id="toggle-check"
                         type="checkbox"
-                        variant="outline-dark"
-                        checked={checked}
-                        value="1"
+                        variant={mission.reserved ? 'info' : 'dark'}
                       >
-                        Join Mission
+                        {mission.reserved ? 'Active member' : statusText}
                       </ToggleButton>
-                    </ButtonGroup>
-                  </td>
+                    </td>
+                    <td>
+                      <ButtonGroup className="mb-2">
+                        <ToggleButton
+                          id="toggle-check"
+                          type="checkbox"
+                          variant={mission.reserved ? 'outline-danger' : 'outline-dark'}
+                          onClick={() => handleJoinMission(mission.mission_id)}
+                        >
+                          {mission.reserved ? 'Leave Mission' : 'Join Mission'}
+                        </ToggleButton>
+                      </ButtonGroup>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="4">No missions available.</td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </Table>
         </div>
