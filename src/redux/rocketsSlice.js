d73354815b0c36ef5747ac40f8457fb892aa7feb
user-faiz <email2faizm@gmail.com>
2023-11-09 08:20:01 +0530

    Use axios and createAsyncThunk to fetch data

--- src/redux/rocketsSlice.js
+++ src/redux/rocketsSlice.js
@@ -1,4 +1,15 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const ROCKET_API = 'https://api.spacexdata.com/v3/rockets';
+
+export const fetchRockets = createAsyncThunk(
+  'rockets/fetchRockets',
+  async () => {
+    const response = await axios.get(ROCKET_API);
+    return response.data;
+  },
+);
 
 const initialState = {};
 
