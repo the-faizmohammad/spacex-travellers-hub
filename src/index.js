17093d3c1a8af3a8bd7b07d8119367b26e9132ad
user-faiz <email2faizm@gmail.com>
2023-11-09 08:45:50 +0530

    fixed linters error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
-import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
