dd193ecde624804390026b4673299010ad6e1c3c
user-faiz <email2faizm@gmail.com>
2023-11-10 12:57:13 +0530

    Update  Myprofile.js

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -12,7 +12,18 @@ const MyProfile = () => {
   const rockets = useSelector(getReservedRockets);
   const rocketStatus = useSelector(getRocketsStatus);
 
-  
+  useEffect(() => {
+    dispatch(myReservedRockets());
+  }, [rocketStatus, dispatch]);
+
+  let renderReserved = '';
+  if (rockets.length) {
+    renderReserved = rockets.map((data) => (
+      <li key={data.id}>
+        <span>{data.rocket_name}</span>
+      </li>
+    ));
+  }
 
   return (
     <div className="result">
@@ -24,7 +35,22 @@ const MyProfile = () => {
           </tbody>
         </table>
       </div>
-      
+      <div className="Box">
+        <h2 className="title">My Missions</h2>
+        <table className="Mission-ProfileTable">
+          <tbody>
+            Coming Soon
+          </tbody>
+        </table>
+      </div>
+      <div className="Box">
+        <h2 className="title">My Dragons</h2>
+        <table className="Mission-ProfileTable">
+          <tbody>
+            Coming Soon
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
