40ac018d489970ac1c17d0514caef9047b52df66
Obimbo07 <obimboausts@gmail.com>
2023-11-10 03:13:47 +0300

    fixed-mission-reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './rocketsSlice';
+import missionReducer from './missionSlice';
 
 const store = configureStore({
   reducer: {
     rockets: rocketReducer,
+    mission: missionReducer,
   },
 });
 
