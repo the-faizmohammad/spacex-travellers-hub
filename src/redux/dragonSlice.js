22d140f250900434dd427eff8a68a3c90bc1e772
Badru Ndawula <badrundawula24@gmail.com>
2023-11-10 21:21:32 +0300

    fix linters

--- src/redux/dragonSlice.js
+++ src/redux/dragonSlice.js
@@ -24,7 +24,7 @@ const dragonSlice = createSlice({
   initialState,
   reducers: {
     reserveDragon: (state, action) => {
-      const dragon = action.payload; 
+      const dragon = action.payload;
       const existingDragon = state.data.find((d) => d.id === dragon.id);
 
       if (existingDragon) {
