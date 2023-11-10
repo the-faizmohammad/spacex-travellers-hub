50836f29df306e404a6fc92910ebf6fc7c81254c
Badru Ndawula <badrundawula24@gmail.com>
2023-11-10 15:01:57 +0300

    fix linter errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,13 +4,10 @@ import rocketReducer from './rocketsSlice';
 import dragonReducer from './dragonSlice';
 import missionReducer from './missionSlice';
 
-
 const store = configureStore({
   reducer: {
     rockets: rocketReducer,
-
     dragon: dragonReducer,
-
     mission: missionReducer,
   },
 });
