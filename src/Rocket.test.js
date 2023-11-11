15ace95c3dec24134685b739c8e4706b2bd596dd
user-faiz <email2faizm@gmail.com>
2023-11-11 13:32:18 +0530

    change import path

--- src/Rocket.test.js
+++ src/Rocket.test.js
@@ -3,7 +3,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 import Rockets from './components/Rockets';
-import { reserveRocket } from './redux/rocketsSlice';
+import { reserveRocket } from './redux/Rockets/rocketsSlice';
 
 const mockStore = configureStore();
 
